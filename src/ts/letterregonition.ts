2e44ea9354ce8fc8d4026121e1d4e7c4c61045c7
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-29 11:47:39 +0200

    style: :sparkles: Draw background image on screen

---
+++ src/ts/letterregonition.ts
@@ -0,0 +1,10 @@
+export class LetterRegonition {
+    constructor() {
+        this.drawBackground();
+    }
+    
+    drawBackground() {
+        document.body.style.backgroundImage = "url(./assets/level1_bg.jpg";
+    }
+}
+
