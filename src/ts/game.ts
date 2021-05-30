f24cf0748cb0b5f5969d5f556831423437a90975
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-05-30 23:17:51 +0200

    fix: :art: Moved addBackground
    Moved addBackground from letterRecognition to Game

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,5 +1,11 @@
 export class game {
   constructor() {
-    console.log("Game running");
+    console.log("Game started");
+
+    this.drawBackground();
+  }
+
+  drawBackground() {
+    document.body.style.backgroundImage = "url(./assets/level1_bg.jpg";
   }
 }
