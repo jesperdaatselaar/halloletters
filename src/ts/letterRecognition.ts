6b354b12b28457221c43562bd0e1f602f940662e
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-31 11:31:47 +0200

    feat: :construction: Added image and word properties to the class

--- src/ts/letterRecognition.ts
+++ src/ts/letterRecognition.ts
@@ -1,7 +1,14 @@
 export class letterRecognition {
 
+    subjectImage : string;
+    word : string;
+
   constructor() {
       console.log("LetterRegonition created");
+      
+      this.subjectImage = "moon_level1.png";
+      this.word = "..oon";
+
       this.create();
   }
 
@@ -11,20 +18,24 @@ export class letterRecognition {
   }
 
   drawSubject() {
+      // Create container for subject image
       let subjectContainer = document.createElement("subjectContainer");
       let subjectImg = document.createElement("img");
       subjectContainer.appendChild(subjectImg);
+      // Draw container on screen
       document.body.appendChild(subjectContainer);
 
-      subjectImg.src = "./assets/moon_level1.png";
+      subjectImg.src = `./assets/${this.subjectImage}`;
   }
 
   drawWord() {
+      // Create container for label
       let labelContainer = document.createElement("labelContainer");
       let wordLabel = document.createElement("label");
       labelContainer.appendChild(wordLabel);
+      // Draw container on screen
       document.body.appendChild(labelContainer);
 
-      wordLabel.innerText = "...aan";
+      wordLabel.innerText = this.word;
   }
 }
