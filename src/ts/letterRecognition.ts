321803fe9cbd6693056974d43248e81b3d8f3edf
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-05-31 10:26:04 +0200

    feat: :construction: Draw moon and label on screen

--- src/ts/letterRecognition.ts
+++ src/ts/letterRecognition.ts
@@ -1,4 +1,30 @@
 export class letterRecognition {
-  constructor() {}
-  //   Add letterRecognition
+
+  constructor() {
+      console.log("LetterRegonition created");
+      this.create();
+  }
+
+  create() {
+      this.drawSubject();
+      this.drawWord();
+  }
+
+  drawSubject() {
+      let subjectContainer = document.createElement("subjectContainer");
+      let subjectImg = document.createElement("img");
+      subjectContainer.appendChild(subjectImg);
+      document.body.appendChild(subjectContainer);
+
+      subjectImg.src = "./assets/moon_level1.png";
+  }
+
+  drawWord() {
+      let labelContainer = document.createElement("labelContainer");
+      let wordLabel = document.createElement("label");
+      labelContainer.appendChild(wordLabel);
+      document.body.appendChild(labelContainer);
+
+      wordLabel.innerText = "...aan";
+  }
 }
