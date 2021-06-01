2e8b8cd508b19f29f17db5a85f2afe77d067f189
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-01 12:51:43 +0200

    feat: :construction: Added gameElement creation

--- src/ts/gameElement.ts
+++ src/ts/gameElement.ts
@@ -1,3 +1,4 @@
+let game = document.querySelector("gamecontainer");
 export interface Element {
   name: string;
   word: string;
@@ -6,7 +7,14 @@ export interface Element {
 }
 
 export class gameElement {
+  div: HTMLElement;
   constructor(el: Element) {
     console.log("New game element");
+    let element = document.createElement("gameElement");
+    element.id = el.name;
+    game?.appendChild(element);
+    let image = document.createElement("img");
+    image.src = el.image || "./assets/logo.png";
+    element.appendChild(image);
   }
 }
