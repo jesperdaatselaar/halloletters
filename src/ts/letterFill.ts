60815f1b58bf2937d6d5dd99a1b9debad2d0ba04
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-07 11:54:24 +0200

    feat: :construction: Check if typed letter is the same as the missing letter

--- src/ts/letterFill.ts
+++ src/ts/letterFill.ts
@@ -3,10 +3,13 @@ let game = document.querySelector("gamecontainer");
 export class LetterFill {
 
     word : string;
+    missingLetter : string;
 
     constructor(data : []) {
         let gameElement = document.querySelector("gameElement");
         gameElement?.classList.add("element-center");
+
+        window.addEventListener("keydown", (e:KeyboardEvent) => this.onKeyDown(e));
 
         this.word = "maan";
     }
@@ -14,6 +17,7 @@ export class LetterFill {
     removeLetter() {
         // Get random letter from word
         let randomLetter = this.word.charAt(Math.floor(Math.random() * this.word.length));
+        this.missingLetter = randomLetter.toUpperCase();
         // Replace the letter with a space
         let newWord = this.word.replace(randomLetter, ' ');
         // Display word with missing letter on screen
@@ -23,5 +27,11 @@ export class LetterFill {
     displayWord(newWord : string) {
         let wordElement = document.createElement("wordElement");
         game?.appendChild(wordElement);
+    }
+
+    private onKeyDown(e: KeyboardEvent): void {
+        if (e.key.toUpperCase() === this.missingLetter) {
+            console.log("Correct awnser");
+        }
     }
 }
