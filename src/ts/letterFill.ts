3da9e723c26566998ac0b5c6ec749504aa0edc20
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-07 11:47:30 +0200

    feat: :construction: Get random letter from word and replace with a space

--- src/ts/letterFill.ts
+++ src/ts/letterFill.ts
@@ -2,15 +2,21 @@ let game = document.querySelector("gamecontainer");
 
 export class LetterFill {
 
-    element : HTMLElement;
+    word : string;
 
-    constructor(data : HTMLElement) {
-        this.element = data;
-        this.element.classList.add("element-center");
-        game?.appendChild(this.element);
+    constructor(data : []) {
+        let gameElement = document.querySelector("gameElement");
+        gameElement?.classList.add("element-center");
+
+        this.word = "maan";
     }
 
-
-
-    
+    removeLetter() {
+        // Get random letter from word
+        let randomLetter = this.word.charAt(Math.floor(Math.random() * this.word.length));
+        // Replace the letter with a space
+        let newWord = this.word.replace(randomLetter, ' ');
+        // Display word with missing letter on screen
+        this.displayWord(newWord);
+    }
 }
