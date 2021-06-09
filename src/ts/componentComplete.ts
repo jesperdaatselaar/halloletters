dcff369ff25988b4c635b0a2790a817d3a6b7dbd
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-09 17:16:42 +0200

    style: :construction: Created ComponentComplete format based of class diagram

---
+++ src/ts/componentComplete.ts
@@ -0,0 +1,12 @@
+import { Assignment } from "./assignment";
+
+export class ComponentComplete {
+  private callbackKeyDown: EventListener;
+  assignment: Assignment;
+
+  constructor() {}
+
+  create(): void {}
+  handleKeyDown(e: Event): void {}
+  remove(): void {}
+}
