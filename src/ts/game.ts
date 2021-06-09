12ba3066217a8cb33557e3fdd35817b957c1c89f
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-09 10:25:52 +0200

    feat: :fire: Removed let root

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,6 +1,5 @@
 import { subjectSelect } from "./subjectSelect.js";
 
-let root: HTMLElement = document.querySelector("gameContainer")!;
 export class game {
   constructor() {
     console.log("Game started");
