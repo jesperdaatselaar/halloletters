e349680b0c2f9759cb2d45368c27599394905915
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-09 14:44:52 +0200

    fix: :fire: Retry

--- src/ts/game.ts
+++
@@ -1,8 +0,0 @@
-import { subjectSelect } from "./subjectSelect.js";
-
-export class game {
-  constructor() {
-    console.log("Game started");
-    new subjectSelect();
-  }
-}
--- src/ts/master.ts
+++
@@ -1,3 +0,0 @@
-import { game } from "./game.js";
-
-new game();
