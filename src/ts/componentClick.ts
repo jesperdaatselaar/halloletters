2ef7765971b482efde48d566c44be2a687945a91
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-09 17:17:11 +0200

    style: :construction: Created ComponentClick format based of class diagram

---
+++ src/ts/componentClick.ts
@@ -0,0 +1,12 @@
+import { Assignment } from "./assignment";
+
+export class ComponentClick {
+  private callbackClick: EventListener;
+  assignment: Assignment;
+
+  constructor() {}
+
+  create(): void {}
+  handleKeyClick(e: Event): void {}
+  remove(): void {}
+}
