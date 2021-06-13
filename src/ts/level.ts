4ef57e4caf3ae6419f6c3e6b2a1884fbd62206f5
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-13 18:22:55 +0200

    feat: :construction: Start new componentClick

--- src/ts/level.ts
+++ src/ts/level.ts
@@ -1,3 +1,5 @@
+import { Assignment } from "./assignment.js";
+import { ComponentClick } from "./componentClick.js";
 import { Theme } from "./interfaces.js";
 import themes from "./themes.js";
 
@@ -10,11 +12,12 @@ export class Level {
 
   constructor() {
     this.selectRandomTheme();
+
+    new ComponentClick(new Assignment(this._theme)); 
   }
 
   private selectRandomTheme() {
     this._theme = themes[Math.floor(Math.random() * themes.length)];
-    console.log(this._theme)
   }
   public next(level: number) {}
 }
