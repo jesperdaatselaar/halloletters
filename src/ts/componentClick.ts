1e8832e3bf1df7fc3b97fe9309eeddfbcbe8b16e
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-13 20:14:47 +0200

    feat: :construction: Moved for loop to create function

--- src/ts/componentClick.ts
+++ src/ts/componentClick.ts
@@ -10,31 +10,31 @@ export class ComponentClick {
   constructor(assignment: Assignment) {
     console.log(assignment)
     this.assignment = assignment;
-
-    for(let c of assignment.theme.components) {
-      this.create(c);
-    }
-
-    game!.style.backgroundImage = `url("${assignment.theme.background}")`;
+    this.create();
   }
 
-  create(component: Component): void {
-    let gameComponent = document.createElement("gameComponent");
-    game?.appendChild(gameComponent);
+  create(): void {
+    // Add background to game
+    game!.style.backgroundImage = `url("${this.assignment.theme.background}")`;
+    // Add all game components
+    for(let component of this.assignment.theme.components) {
+      let gameComponent = document.createElement("gameComponent");
+      game?.appendChild(gameComponent);
 
-    gameComponent.id = component.name;
-    gameComponent.dataset.word = component.word;
-    gameComponent.style.backgroundImage = `url("${
-      component.image || "./assets/logo.png"
-    }")`;
+      gameComponent.id = component.name;
+      gameComponent.dataset.word = component.word;
+      gameComponent.style.backgroundImage = `url("${
+        component.image || "./assets/logo.png"
+      }")`;
 
-    let x = Math.floor(
-      Math.random() * (window.innerWidth - gameComponent.clientWidth)
-    );
-    let y = Math.floor(
-      Math.random() * (window.innerHeight - gameComponent.clientHeight)
-    );
-    gameComponent.style.transform = `translate(${x}px, ${y}px)`;
+      let x = Math.floor(
+        Math.random() * (window.innerWidth - gameComponent.clientWidth)
+      );
+      let y = Math.floor(
+        Math.random() * (window.innerHeight - gameComponent.clientHeight)
+      );
+      gameComponent.style.transform = `translate(${x}px, ${y}px)`;
+    }
   }
   handleKeyClick(e: Event): void {}
   remove(): void {}
