2b26d7b9b2db345bd99133bb804b921c9e43175a
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-13 18:22:21 +0200

    feat: :construction: Display components and background on screen

--- src/ts/componentClick.ts
+++ src/ts/componentClick.ts
@@ -1,12 +1,41 @@
 import { Assignment } from "./assignment";
+import { Component } from "./interfaces";
+
+let game = document.querySelector("game") as HTMLElement;
 
 export class ComponentClick {
   private callbackClick: EventListener;
   assignment: Assignment;
 
-  constructor() {}
+  constructor(assignment: Assignment) {
+    console.log(assignment)
+    this.assignment = assignment;
 
-  create(): void {}
+    for(let c of assignment.theme.components) {
+      this.create(c);
+    }
+
+    game!.style.backgroundImage = `url("${assignment.theme.background}")`;
+  }
+
+  create(component: Component): void {
+    let gameComponent = document.createElement("gameComponent");
+    game?.appendChild(gameComponent);
+
+    gameComponent.id = component.name;
+    gameComponent.dataset.word = component.word;
+    gameComponent.style.backgroundImage = `url("${
+      component.image || "./assets/logo.png"
+    }")`;
+
+    let x = Math.floor(
+      Math.random() * (window.innerWidth - gameComponent.clientWidth)
+    );
+    let y = Math.floor(
+      Math.random() * (window.innerHeight - gameComponent.clientHeight)
+    );
+    gameComponent.style.transform = `translate(${x}px, ${y}px)`;
+  }
   handleKeyClick(e: Event): void {}
   remove(): void {}
 }
