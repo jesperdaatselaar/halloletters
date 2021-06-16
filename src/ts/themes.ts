151515f34a50527105c7245a41cd34de51cd505e
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-16 10:25:02 +0200

    fix: :truck: Moved and renamed files in assets folder

--- src/ts/themes.ts
+++ src/ts/themes.ts
@@ -1,50 +1,74 @@
 export default [
-    {
-      name: "space",
-      components: [
-        {
-          name: "moon",
-          word: "maan",
-          image: "./assets/moon.png",
-          sound: "/src/sounds/moon_m.png",
-        },
-        {
-          name: "rocket",
-          word: "raket",
-          image: "./assets/rocket.png",
-          sound: "/src/sounds/moon_m.png",
-        },
-        {
-          name: "astronaut",
-          word: "astronaut",
-          image: "./assets/astronaut.png",
-          sound: "/src/sounds/moon_m.png",
-        },
-      ],
-      background: "./assets/background.png",
-    },
-    {
-      name: "farm",
-      components: [
-        {
-          name: "cow",
-          word: "koe",
-          image: "./assets/cow.png",
-          sound: "/src/sounds/cow_k.png",
-        },
-        {
-          name: "sheep",
-          word: "schaap",
-          image: "./assets/sheep.png",
-          sound: "/src/sounds/sheep_s.png",
-        },
-        {
-          name: "farm",
-          word: "boerderij",
-          image: "./assets/farm.png",
-          sound: "/src/sounds/farm_b.png",
-        },
-      ],
-      background: "./assets/background2.png",
-    },
-  ];
+  {
+    name: "space",
+    components: [
+      {
+        name: "moon",
+        word: "maan",
+        image: "./assets/space/moon.png",
+        sound: "/src/sounds/moon_m.png",
+      },
+      {
+        name: "rocket",
+        word: "raket",
+        image: "./assets/space/rocket.png",
+        sound: "/src/sounds/moon_m.png",
+      },
+      {
+        name: "astronaut",
+        word: "astronaut",
+        image: "./assets/space/astronaut.png",
+        sound: "/src/sounds/moon_m.png",
+      },
+    ],
+    background: "./assets/space/background.png",
+  },
+  {
+    name: "farm",
+    components: [
+      {
+        name: "cow",
+        word: "koe",
+        image: "./assets/farm/cow.png",
+        sound: "/src/sounds/cow_k.png",
+      },
+      {
+        name: "sheep",
+        word: "schaap",
+        image: "./assets/farm/sheep.png",
+        sound: "/src/sounds/sheep_s.png",
+      },
+      {
+        name: "farm",
+        word: "boerderij",
+        image: "./assets/farm/farm.png",
+        sound: "/src/sounds/farm_b.png",
+      },
+    ],
+    background: "./assets/farm/background.png",
+  },
+  {
+    name: "forest",
+    components: [
+      {
+        name: "acorn",
+        word: "eikel",
+        image: "./assets/forest/acorn.png",
+        sound: "/src/sounds/acorn_a.png",
+      },
+      {
+        name: "bush",
+        word: "bosje",
+        image: "./assets/forest/bush.png",
+        sound: "/src/sounds/bosje_b.png",
+      },
+      {
+        name: "twig",
+        word: "takje",
+        image: "./assets/forest/twig.png",
+        sound: "/src/sounds/twig_t.png",
+      },
+    ],
+    background: "./assets/forest/background.png",
+  },
+];
