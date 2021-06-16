ba50b10b0840016ea54fcca4dac75aadf8b5dbd8
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-16 13:52:00 +0200

    feat: :sparkles: Add gameText and keypress handler for word game

--- src/ts/componentComplete.ts
+++ src/ts/componentComplete.ts
@@ -9,6 +9,8 @@ export class ComponentComplete {
   constructor(assignment: Assignment) {
     this.assignment = assignment;
     this.correctAnswer = assignment.correctAnswer;
+    this.callbackKeyDown = (e: Event) => this.handleKeyDown(e as KeyboardEvent);
+    window.addEventListener("keydown", this.callbackKeyDown);
     this.create();
   }
 
@@ -17,7 +19,15 @@ export class ComponentComplete {
     game.style.backgroundImage = `url("${this.assignment.theme.background}")`;
     // Add all game components
     let gameComponent = document.createElement("gameComponent");
+    let gameText = document.createElement("gameText");
+    gameText.innerHTML = this.correctAnswer.component.word.replace(
+      this.correctAnswer.letter.toLowerCase(),
+      " _ "
+    );
+
+    console.log(this.correctAnswer.letter);
     game?.appendChild(gameComponent);
+    game?.appendChild(gameText);
 
     gameComponent.id = this.correctAnswer.component.name;
     gameComponent.dataset.word = this.correctAnswer.component.word;
@@ -28,6 +38,16 @@ export class ComponentComplete {
 
     gameComponent.style.backgroundImage = `url("${this.correctAnswer.component.image}")`;
   }
-  handleKeyDown(e: Event): void {}
-  remove(): void {}
+  handleKeyDown(e: KeyboardEvent): void {
+    if (e.key.toUpperCase() === this.correctAnswer.letter) {
+      console.log("GG WP");
+      window.removeEventListener("keydown", this.callbackKeyDown);
+      this.remove();
+    }
+  }
+  remove(): void {
+    document.querySelector("gameComponent")?.remove();
+    document.querySelector("gameText")?.remove();
+    this.assignment.next();
+  }
 }
