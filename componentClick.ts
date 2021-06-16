caf7c16773be5f8dc85809c500e5bc6a774b78eb
Dancingkoalaa <75275707+Dancingkoalaa@users.noreply.github.com>
2021-06-16 11:40:51 +0200

    Added themes, added code for sounds

---
+++ componentClick.ts
@@ -0,0 +1,85 @@
+import { Assignment } from "./assignment";
+import { Component } from "./interfaces";
+
+let game = document.querySelector("game") as HTMLElement;
+
+export class ComponentClick {
+  private callbackClick: EventListener;
+  assignment: Assignment;
+
+  constructor(assignment: Assignment) {
+    console.log(assignment);
+    this.assignment = assignment as Assignment;
+    this.create();
+  }
+
+  create(): void {
+    // Add background to game
+    game.style.backgroundImage = `url("${this.assignment.theme.background}")`;
+    // Add all game components
+    for (let component of this.assignment.theme.components) {
+      let gameComponent = document.createElement("gameComponent");
+      game?.appendChild(gameComponent);
+
+      gameComponent.id = component.name;
+      gameComponent.dataset.word = component.word;
+
+      this.setPosition(gameComponent);
+
+      for (let child of game.children) {
+        this.checkPosition(child, gameComponent);
+      }
+
+      gameComponent.style.backgroundImage = `url("${
+        component.image || "./assets/logo.png"
+      }")`;
+    }
+    const playsound = new Audio (`"${this.assignment.correctAnswer.component.sound}"`);
+    console.log(this.assignment.correctAnswer.component.sound);
+    playsound.play
+  }
+  handleKeyClick(e: Event): void {}
+  remove(): void {}
+  collisionDetect(a: ClientRect, b: ClientRect): Boolean {
+    return (
+      a.left <= b.right &&
+      b.left <= a.right &&
+      a.top <= b.bottom &&
+      b.top <= a.bottom
+    );
+  }
+  checkPosition(child: Element, gameComponent: HTMLElement) {
+    let overlay;
+    if (child != gameComponent) {
+      overlay = this.collisionDetect(
+        child.getBoundingClientRect(),
+        gameComponent.getBoundingClientRect()
+      );
+    }
+
+    while (overlay) {
+      console.log("in while");
+
+      this.setPosition(gameComponent);
+      if (child != gameComponent) {
+        overlay = this.collisionDetect(
+          child.getBoundingClientRect(),
+          gameComponent.getBoundingClientRect()
+        );
+      }
+    }
+  }
+
+  private setPosition(gameComponent: HTMLElement) {
+    console.log("Change position");
+
+    let x = Math.floor(
+      Math.random() * (window.innerWidth - gameComponent.clientWidth)
+    );
+    let y = Math.floor(
+      Math.random() * (window.innerHeight - gameComponent.clientHeight)
+    );
+
+    gameComponent.style.transform = `translate(${x}px, ${y}px)`;
+  }
+}
---
+++ themes.ts
@@ -0,0 +1,74 @@
+export default [
+  {
+    name: "space",
+    components: [
+      {
+        name: "moon",
+        word: "maan",
+        image: "./assets/space/moon.png",
+        sound: "./sounds/space/moon.mp3",
+      },
+      {
+        name: "rocket",
+        word: "raket",
+        image: "./assets/space/rocket.png",
+        sound: "./sounds/space/rocket.mp3",
+      },
+      {
+        name: "astronaut",
+        word: "astronaut",
+        image: "./assets/space/astronaut.png",
+        sound: "./sounds/space/astronaut.mp3",
+      },
+    ],
+    background: "./assets/space/background.png",
+  },
+  {
+    name: "farm",
+    components: [
+      {
+        name: "cow",
+        word: "koe",
+        image: "./assets/farm/cow.png",
+        sound: "./sounds/farm/cow.mp3",
+      },
+      {
+        name: "sheep",
+        word: "schaap",
+        image: "./assets/farm/sheep.png",
+        sound: "./sounds/farm/sheep.mp3",
+      },
+      {
+        name: "farm",
+        word: "boerderij",
+        image: "./assets/farm/farm.png",
+        sound: "./sounds/farm/barn.mp3",
+      },
+    ],
+    background: "./assets/farm/background.png",
+  },
+  {
+    name: "forest",
+    components: [
+      {
+        name: "acorn",
+        word: "eikel",
+        image: "./assets/forest/acorn.png",
+        sound: "/src/sounds/acorn_a.png",
+      },
+      {
+        name: "bush",
+        word: "bosje",
+        image: "./assets/forest/bush.png",
+        sound: "/src/sounds/bosje_b.png",
+      },
+      {
+        name: "twig",
+        word: "takje",
+        image: "./assets/forest/twig.png",
+        sound: "/src/sounds/twig_t.png",
+      },
+    ],
+    background: "./assets/forest/background.png",
+  },
+];
