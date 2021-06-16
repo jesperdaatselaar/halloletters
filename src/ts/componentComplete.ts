6cf0e85554167ba7ae25beba3829eb7659d7ded2
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-06-16 11:19:26 +0200

    feat: :construction: Started under construction function

--- src/ts/componentComplete.ts
+++ src/ts/componentComplete.ts
@@ -1,15 +1,31 @@
 import { Assignment } from "./assignment";
+import { Answer } from "./interfaces";
 
 export class ComponentComplete {
   private callbackKeyDown: EventListener;
   assignment: Assignment;
+  correctAnswer: Answer;
 
   constructor(assignment: Assignment) {
     this.assignment = assignment;
+    this.correctAnswer = assignment.correctAnswer;
     this.create();
   }
 
-  create(): void {}
+  create(): void {
+    let game = document.querySelector("game") as HTMLElement;
+    game.style.backgroundImage = `url("${this.assignment.theme.background}")`;
+    // Add all game components
+    let gameComponent = document.createElement("gameComponent");
+    game?.appendChild(gameComponent);
+
+    gameComponent.id = this.correctAnswer.component.name;
+    gameComponent.dataset.word = this.correctAnswer.component.word;
+
+    gameComponent.style.transform = `translate(50vw, 50vh)`;
+
+    gameComponent.style.backgroundImage = `url("${this.correctAnswer.component.image}")`;
+  }
   handleKeyDown(e: Event): void {}
   remove(): void {}
 }
