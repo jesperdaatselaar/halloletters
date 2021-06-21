b82d5f4bea4abc7761e77bf72afc582ee8301b6a
Kasper Beljaars <kasperbeljaars@hotmail.com>
2021-06-21 11:12:29 +0200

    feat: :construction: Added complete level audio

--- src/ts/componentComplete.ts
+++ src/ts/componentComplete.ts
@@ -1,16 +1,25 @@
 import { Assignment } from "./assignment";
 import { Answer } from "./interfaces";
 
+let levelCompleteAudio = new Audio("./sounds/level-complete.mp3");
+
 export class ComponentComplete {
   private callbackKeyDown: EventListener;
+  private callAudioDone : EventListener;
+  private gameText : HTMLElement;
   assignment: Assignment;
   correctAnswer: Answer;
 
   constructor(assignment: Assignment) {
     this.assignment = assignment;
     this.correctAnswer = assignment.correctAnswer;
+
     this.callbackKeyDown = (e: Event) => this.handleKeyDown(e as KeyboardEvent);
     window.addEventListener("keydown", this.callbackKeyDown);
+
+    this.callAudioDone = () => this.handleAudioEventListener();
+    levelCompleteAudio.addEventListener("ended", this.callAudioDone);
+
     this.create();
   }
 
@@ -19,15 +28,15 @@ export class ComponentComplete {
     game.style.backgroundImage = `url("${this.assignment.theme.background}")`;
     // Add all game components
     let gameComponent = document.createElement("gameComponent");
-    let gameText = document.createElement("gameText");
-    gameText.innerHTML = this.correctAnswer.component.word.replace(
+    this.gameText = document.createElement("gameText");
+    this.gameText.innerHTML = this.correctAnswer.component.word.replace(
       this.correctAnswer.letter.toLowerCase(),
       " _ "
     );
 
     console.log(this.correctAnswer.letter);
     game?.appendChild(gameComponent);
-    game?.appendChild(gameText);
+    game?.appendChild(this.gameText);
 
     gameComponent.id = this.correctAnswer.component.name;
     gameComponent.dataset.word = this.correctAnswer.component.word;
@@ -40,14 +49,20 @@ export class ComponentComplete {
   }
   handleKeyDown(e: KeyboardEvent): void {
     if (e.key.toUpperCase() === this.correctAnswer.letter) {
-      console.log("GG WP");
       window.removeEventListener("keydown", this.callbackKeyDown);
-      this.remove();
+      // Replcae the missing letter with the right one
+      this.gameText.innerHTML = this.correctAnswer.component.word;
+      // Play complete levelCompleteAudio
+      levelCompleteAudio.play();
     }
   }
   remove(): void {
     document.querySelector("gameComponent")?.remove();
     document.querySelector("gameText")?.remove();
+  }
+
+  private handleAudioEventListener() {
+    this.remove();
     this.assignment.next();
   }
 }
